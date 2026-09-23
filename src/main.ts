c95539231b6e178c95d6d14a1c355b90f20a594a
TalhaShahidKhan <talhashahidkhan49@gmail.com>
2026-09-23 22:37:19 +0600

    Prepared for Vercel

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import { AppModule } from './app.module.js';
 
+const server = express();
+let cachedApp: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    app.setGlobalPrefix('api');
+    app.enableCors(); // Added CORS to allow requests from the frontend
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+    await app.init();
+    cachedApp = app;
+  }
+  return server;
 }
-await bootstrap();
+
+// Support for local development
+if (!process.env.VERCEL) {
+  bootstrap().then(() => {
+    server.listen(process.env.PORT ?? 3000, () => {
+      console.log(`Server listening on port ${process.env.PORT ?? 3000}`);
+    });
+  });
+}
+
+// Export for Vercel Serverless
+export default async function handler(req: any, res: any) {
+  const app = await bootstrap();
+  return app(req, res);
+}
